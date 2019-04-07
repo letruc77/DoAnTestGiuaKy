81dd4507380776d378ced04aa8ef0028b0a68c0a
letruc77 <44826071+letruc77@users.noreply.github.com>
2019-04-07 12:17:34 +0700

    Add new car

--- UnitTestDoAnGiuaKy/UnitTest1.cs
+++ UnitTestDoAnGiuaKy/UnitTest1.cs
@@ -5,13 +5,48 @@ using DoAnWeb.Controllers;
 using DoAnWeb.Areas;
 using DoAnWeb.Models;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace UnitTestDoAnGiuaKy
 {
     [TestClass]
     public class UnitTest1
     {
+        //string connectionString = "metadata=res://*/DoAnWeb.csdl|res://*/DoAnWeb.ssdl|res://*/DoAnWeb.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=DESKTOP-297T8GT;initial catalog=DoAnNganh;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        //string connectionString = "data source=DESKTOP-297T8GT;initial catalog=DoAnNganh;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+        //public virtual void OpenConectionString(string con)
+        //{
+        //    using (var connction = new SqlConnection(con))
+        //    {
+        //        connction.Open();
+        //    }
+        //}
         #region Test Login
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            string ConnectionString = @"data source=DESKTOP-297T8GT;initial catalog=DoAnNganh;" +
+                "integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+        }
+        [TestMethod]
+        //[TestInitialize]
+        public void TestLogin()
+        {
+            //OpenConectionString(connectionString);
+            string username = "admin";
+            string password = "admin2019";
+            DoAnWeb.NguoiDung nd = new NguoiDung();
+            nd.TenNguoiDung = username;
+            nd.PassWord = password; ;
+            DoAnWeb.Areas.Admin.Controllers.AdminLoginController ctrl = new DoAnWeb.Areas.Admin.Controllers.AdminLoginController();
+            var result = ctrl.AdminLogin(nd) as JsonResult;
+            var mes = result.Data.ToString();
+            Assert.AreEqual("success", mes);
+        }
+
+
+
+
         private String GetMD5(string txt)
         {
             String str = "";
@@ -24,19 +59,7 @@ namespace UnitTestDoAnGiuaKy
             }
             return str;
         }
-        [TestMethod]
-        public void TestLogin()
-        {
-            string username = "admin";
-            string password = "admin2019";
-            DoAnWeb.NguoiDung nd = new NguoiDung();
-            nd.TenNguoiDung = username;
-            nd.PassWord = password; ;
-            DoAnWeb.Areas.Admin.Controllers.AdminLoginController ctrl = new DoAnWeb.Areas.Admin.Controllers.AdminLoginController();
-            var result = ctrl.AdminLogin(nd) as JsonResult;
-            var mes = result.Data.ToString();
-            Assert.AreEqual("success", mes);
-        }
+        
         [TestMethod]
         public void TestLoginErrorPassword()
         {
@@ -96,9 +119,21 @@ namespace UnitTestDoAnGiuaKy
         {
             DoAnWeb.Xe xe = new Xe();
             DoAnWeb.Areas.Admin.Controllers.ProductsController ctrl = new DoAnWeb.Areas.Admin.Controllers.ProductsController();
+            xe.ChiTiet = "Unit Test";
+            xe.CreatedDate = DateTime.Now;
+            xe.GiaNiemYetXe = 10000;
+            xe.GiaVonXe = 5000;
+            xe.Hinh1 = "banner.jpg";
+            xe.Hinh2 = "banner1.jpg";
+            xe.Hinh3 = "banner2.jpg";
+            xe.IdKhoXe = Guid.Parse("ACBA9D50-0AC8-47B3-BE54-B2EC5DD5686C");
+            xe.IdNhaCungCap = Guid.Parse("F3409D77-8847-43BA-B408-2C51470B2FAC");
+            xe.MaLoaiXe = Guid.Parse("89E51C6D-C3F2-4448-B819-100BF449F70F");
+            xe.SoMay = "SM123456";
+            xe.SoKhung = "SK123456";
             var result = ctrl.ThemXe(xe,"");
             var mes = result.Data.ToString();
-            Assert.AreEqual("success", mes);
+            Assert.AreEqual("Bạn đã thêm xe thành công.", mes);
         }
         #endregion
     }
